4050f17963d32b57cd2b3403dc84376cb80c553a
Daniel Johannesson <daniel@johannesson.cz>
2021-01-03 16:42:13 +0100

    home-page TESTING

--- pages/index.js
+++ pages/index.js
@@ -70,15 +70,62 @@ import React, { useState, useEffect } from 'react';
 
 export default function Home(props) {
   useEffect(() => {
-    // Update the document title using the browser API
-    console.log(process.env.HELLOWORLD);
-  });
+    if(typeof window !== 'undefined'){
+      const M = require('materialize-css');
+      var sidenav = document.querySelectorAll(".sidenav");
+      M.Sidenav.init(sidenav, {});
+      
+        var elems = document.querySelectorAll('.dropdown-trigger');
+        M.Dropdown.init(elems, {});
+      
+      
+    }
+  }, [ ]);
+  // useEffect(() => {
+  //   // Update the document title using the browser API
+  //   console.log(process.env.HELLOWORLD);
+    
+  // });
   return (
     <>
       {props.ssrWorking ? (
+        
         <div>
           <img src="/assets/success.jpg" height="500" />
           <h2> Deployment Successful of Nextjs Application with SSR on Firebase. </h2>
+          <a className="waves-effect waves-light btn">buttonghghg</a>
+          <a className="waves-effect waves-light btn"><i className="material-icons left">cloud</i>buttohghghn</a>
+        
+  <a className='dropdown-trigger btn' href='#' data-target='dropdown1'>Drop Me!</a>
+
+  
+  <ul id='dropdown1' className='dropdown-content'>
+    <li><a href="#!">one</a></li>
+    <li><a href="#!">two</a></li>
+    <li className="divider" tabIndex="-1"></li>
+    <li><a href="#!">three</a></li>
+    <li><a href="#!"><i className="material-icons">view_module</i>four</a></li>
+    <li><a href="#!"><i className="material-icons">cloud</i>five</a></li>
+  </ul>
+
+  <nav> hu </nav>
+
+<ul id="slide-out" className="sidenav">
+  <li><div className="user-view">
+    <div className="background">
+      <img src="images/office.jpg"/>
+    </div>
+    <a href="#user"><img className="circle" src="images/yuna.jpg"/></a>
+    <a href="#name"><span className="white-text name">John Doe</span></a>
+    <a href="#email"><span className="white-text email">jdandturk@gmail.com</span></a>
+  </div></li>
+  <li><a href="#!"><i className="material-icons">cloud</i>First Link With Icon</a></li>
+  <li><a href="#!">Second Link</a></li>
+  <li><div className="divider"></div></li>
+  <li><a className="subheader">Subheader</a></li>
+  <li><a className="waves-effect" href="#!">Third Link With Waves</a></li>
+</ul>
+<a href="#" data-target="slide-out" className="sidenav-trigger"><i className="material-icons">menu</i></a>
         </div>
       ) : (
         <h2>SSR not working</h2>
