0d07229eaab62804b6d26e227d2b062ae5a13174
Daniel Johannesson <daniel@johannesson.cz>
2021-01-03 16:40:39 +0100

    added file for body-tag

---
+++ pages/_document.js
@@ -0,0 +1,35 @@
+import Document, { Html, Head, Main, NextScript } from 'next/document'
+
+class MyDocument extends Document {
+  static async getInitialProps(ctx) {
+    const initialProps = await Document.getInitialProps(ctx)
+    return { ...initialProps }
+  }
+
+  render() {
+    return (
+      <Html>
+        <Head />
+        <body>
+          <Main />
+          <NextScript />
+          {/* <!-- Firebase App (the core Firebase SDK) is always required and must be listed first --> */}
+			    <script src="/__/firebase/8.1.2/firebase-app.js"></script>
+
+          {/* <!-- If you enabled Analytics in your project, add the Firebase SDK for Analytics --> */}
+          <script src="/__/firebase/8.1.2/firebase-analytics.js"></script>
+
+          {/* <!-- Add Firebase products that you want to use --> */}
+          <script src="/__/firebase/8.1.2/firebase-auth.js"></script>
+          <script src="/__/firebase/8.1.2/firebase-firestore.js"></script>
+          <script src="/__/firebase/8.1.2/firebase-storage.js"></script>
+          
+          {/* <!-- Initialize Firebase --> */}
+          <script src="/__/firebase/init.js"></script>
+        </body>
+      </Html>
+    )
+  }
+}
+
+export default MyDocument
