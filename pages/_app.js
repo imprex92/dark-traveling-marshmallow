91150f6a8961bca0f46174578ef1e7e9853e23c2
Daniel Johannesson <daniel@johannesson.cz>
2021-01-03 16:40:08 +0100

    added materialize icon + jquery

--- pages/_app.js
+++ pages/_app.js
@@ -1,29 +1,23 @@
 import Head from 'next/head'
 import '../styles/globals.css'
+import '../styles/materialize.css'
+
 
 function MyApp({Component,pageProps}) {
   return (
-      <>
-          <Head>
-              <script 
-                  src="https://code.jquery.com/jquery-3.5.1.slim.min.js" 
-                  crossorigin="anonymous"
-                  />
-                  {/* <!-- Firebase App (the core Firebase SDK) is always required and must be listed first --> */}
-              <script src="/__/firebase/8.1.2/firebase-app.js"></script>
-
-              {/* <!-- If you enabled Analytics in your project, add the Firebase SDK for Analytics --> */}
-              <script src="/__/firebase/8.1.2/firebase-analytics.js"></script>
-
-              {/* <!-- Add Firebase products that you want to use --> */}
-              <script src="/__/firebase/8.1.2/firebase-auth.js"></script>
-              <script src="/__/firebase/8.1.2/firebase-firestore.js"></script>
-              <script src="/__/firebase/8.1.2/firebase-storage.js"></script>
-              {/* <!-- Initialize Firebase --> */}
-              <script src="/__/firebase/init.js"></script>
-          </Head>
-          <Component {...pageProps}/>
-      </>
+	  <>
+		<Head>
+			<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet"/>
+			{/* <link type="text/css" rel="stylesheet" href="../styles/materialize.css"  media="screen,projection"/> */}
+			<meta name="viewport" content="width=device-width, initial-scale=1"></meta>
+			<script 
+				src="https://code.jquery.com/jquery-3.5.1.slim.min.js" 
+				crossOrigin="anonymous"
+				/>
+				
+		</Head>
+		<Component {...pageProps}/>
+	  </>
   );
 }
 
