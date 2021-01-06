0e9b6bb9dad225792047102faf414c17d08dca1b
Daniel Johannesson <daniel@johannesson.cz>
2021-01-06 21:34:23 +0100

    Firebase Reserver ULS's commented out temporarly

--- pages/_document.js
+++ pages/_document.js
@@ -14,18 +14,18 @@ class MyDocument extends Document {
           <Main />
           <NextScript />
           {/* <!-- Firebase App (the core Firebase SDK) is always required and must be listed first --> */}
-			    <script src="/__/firebase/8.1.2/firebase-app.js"></script>
+			    {/* <script src="/__/firebase/8.1.2/firebase-app.js"></script> */}
 
           {/* <!-- If you enabled Analytics in your project, add the Firebase SDK for Analytics --> */}
-          <script src="/__/firebase/8.1.2/firebase-analytics.js"></script>
+          {/* <script src="/__/firebase/8.1.2/firebase-analytics.js"></script> */}
 
           {/* <!-- Add Firebase products that you want to use --> */}
-          <script src="/__/firebase/8.1.2/firebase-auth.js"></script>
+          {/* <script src="/__/firebase/8.1.2/firebase-auth.js"></script>
           <script src="/__/firebase/8.1.2/firebase-firestore.js"></script>
-          <script src="/__/firebase/8.1.2/firebase-storage.js"></script>
+          <script src="/__/firebase/8.1.2/firebase-storage.js"></script> */}
           
           {/* <!-- Initialize Firebase --> */}
-          <script src="/__/firebase/init.js"></script>
+          {/* <script src="/__/firebase/init.js"></script> */}
         </body>
       </Html>
     )
