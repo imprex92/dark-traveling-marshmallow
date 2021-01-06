14ebcf27261b046968df2c6e7fbe859005f22dc9
Daniel Johannesson <daniel@johannesson.cz>
2021-01-06 21:27:42 +0100

    added authProvider.context global + materialize

--- pages/_app.js
+++ pages/_app.js
@@ -1,22 +1,28 @@
 import Head from 'next/head'
+import { AuthProvider } from '../contexts/AuthContext'
 import '../styles/globals.css'
 import '../styles/materialize.css'
 
+if(typeof window !== 'undefined'){
+	require( '../js/materialize')
+	}
 
 function MyApp({Component,pageProps}) {
   return (
 	  <>
-		<Head>
-			<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet"/>
-			{/* <link type="text/css" rel="stylesheet" href="../styles/materialize.css"  media="screen,projection"/> */}
-			<meta name="viewport" content="width=device-width, initial-scale=1"></meta>
-			<script 
-				src="https://code.jquery.com/jquery-3.5.1.slim.min.js" 
-				crossOrigin="anonymous"
-				/>
-				
-		</Head>
-		<Component {...pageProps}/>
+	  	<AuthProvider>
+			<Head>
+				<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet"/>
+				{/* <link type="text/css" rel="stylesheet" href="../styles/materialize.css"  media="screen,projection"/> */}
+				<meta name="viewport" content="width=device-width, initial-scale=1"></meta>
+				<script 
+					src="https://code.jquery.com/jquery-3.5.1.slim.min.js" 
+					crossOrigin="anonymous"
+					/>
+					
+			</Head>
+			<Component {...pageProps}/>
+		</AuthProvider>
 	  </>
   );
 }
