b5f2e163d08cbd5ade6e4bf5bddff6ec76369f12
Daniel Johannesson <daniel@johannesson.cz>
2021-01-10 20:38:57 +0100

    login logout, change userdat functions added

--- contexts/AuthContext.js
+++ contexts/AuthContext.js
@@ -11,10 +11,12 @@ export function useAuth(){
 
 export function AuthProvider({children}) {
 	const [currentUser, setCurrentUser] = useState()
+	const [isLoading, setIsLoading] = useState(true)
 
 	useEffect(() => {
 		const unsubscribe = projectAuth.onAuthStateChanged( user => {
 			setCurrentUser(user)
+			setIsLoading(false)
 		})
 		return unsubscribe
 	}, [])
@@ -23,15 +25,71 @@ export function AuthProvider({children}) {
 		return projectAuth.createUserWithEmailAndPassword(email, password)
 	}
 
+	function login(email, password){
+		return projectAuth.signInWithEmailAndPassword(email, password)
+	}
+
+	function updateUserInfo(userName, userAvatarURL){
+		return projectAuth.currentUser.updateProfile({
+			displayName: projectAuth.currentUser.displayName || userName,
+			photoURL: projectAuth.currentUser.photoURL || userAvatarURL
+		})
+	}
+
+
+	function loginWithGoogle() {
+		console.log('inside context googleauth');
+		let provider = new firebase.auth.GoogleAuthProvider();
+		
+
+		projectAuth
+			.useDeviceLanguage()
+			.signInWithRedirect(provider)
+			.getRedirectResult()
+			.then((result) => {
+				console.log('auth is running');
+				if (result.credential) {
+				/** @type {firebase.auth.OAuthCredential} */
+				var credential = result.credential;
+
+				// This gives you a Google Access Token. You can use it to access the Google API.
+				var token = credential.accessToken;
+				
+				}
+				// The signed-in user info.
+				var user = result.user;
+				console.log(user, token);
+				
+			}).catch((error) => {
+				// Handle Errors here.
+				var errorCode = error.code;
+				var errorMessage = error.message;
+				// The email of the user's account used.
+				var email = error.email;
+				// The firebase.auth.AuthCredential type that was used.
+				var credential = error.credential;
+				console.error(errorCode, errorMessage);
+			});
+
+		
+	}
+
+	function logout(){
+		return projectAuth.signOut()
+	}
 	
 	const value = {
 		currentUser,
-		signup
+		signup,
+		login,
+		logout,
+		loginWithGoogle,
+		updateUserInfo
 	}
 
 	return (
 		<AuthContext.Provider value={value}>
-			{children}
+			{!isLoading && children}
 		</AuthContext.Provider>
 	)
 }
