d37eb7e1b932784fa2ae41db526eacbba2eb79d5
Daniel Johannesson <daniel@johannesson.cz>
2021-01-16 17:17:53 +0100

    databaseContext added

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 import { AuthProvider } from '../contexts/AuthContext'
+import { DatabaseProvider } from '../contexts/DatabaseContext'
 import '../styles/globals.css'
 import '../styles/materialize.css'
 import '../styles/signin.style.css'
@@ -12,17 +13,19 @@ function MyApp({Component,pageProps}) {
   return (
 	  <>
 	  	<AuthProvider>
-			<Head>
-				<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet"/>
-				{/* <link type="text/css" rel="stylesheet" href="../styles/materialize.css"  media="screen,projection"/> */}
-				<meta name="viewport" content="width=device-width, initial-scale=1"></meta>
-				<script 
-					src="https://code.jquery.com/jquery-3.5.1.slim.min.js" 
-					crossOrigin="anonymous"
-					/>
-					
-			</Head>
-			<Component {...pageProps}/>
+			<DatabaseProvider>
+				<Head>
+					<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet"/>
+					{/* <link type="text/css" rel="stylesheet" href="../styles/materialize.css"  media="screen,projection"/> */}
+					<meta name="viewport" content="width=device-width, initial-scale=1"></meta>
+					<script 
+						src="https://code.jquery.com/jquery-3.5.1.slim.min.js" 
+						crossOrigin="anonymous"
+						/>
+						
+				</Head>
+				<Component {...pageProps}/>
+			</DatabaseProvider>
 		</AuthProvider>
 	  </>
   );
