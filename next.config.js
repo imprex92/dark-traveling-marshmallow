cc1b99b4731dd6f6d0a60efdaaed8534f6ebe6c0
Daniel Johannesson <daniel@johannesson.cz>
2021-02-03 18:24:06 +0100

    imported webpack? env?

--- next.config.js
+++ next.config.js
@@ -1,12 +1,17 @@
-// module.exports = {
-	
-//   }
+require('dotenv').config()
+const webpack = require('webpack')
 
 module.exports = {
 	images: {
 		domains: ['lh3.googleusercontent.com'],
 	  },
 	webpack(config) {
+		config.node = {
+			fs: 'empty'
+		  }
+		config.plugins.push(
+			new webpack.EnvironmentPlugin(process.env)
+		  )
 	  config.module.rules.push({
 		test: /\.svg$/,
 		use: ["@svgr/webpack"]
