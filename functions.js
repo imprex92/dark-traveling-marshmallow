b12cc20bfbad7c502deb9d39cd3cc056d959323f
Daniel Johannesson <daniel@johannesson.cz>
2021-02-19 16:40:22 +0100

    cleanup unused code

--- functions.js
+++ functions.js
@@ -5,6 +5,8 @@ const config = functions.config()
 const { default: next } = require('next');
 const isDev = process.env.NODE_ENV !== 'production';
 const express = require('express');
+const { createServer } = require('http')
+const { parse } = require('url')
 const app = express();
 
 
@@ -19,57 +21,15 @@ exports.nextServer = https.onRequest((req, res) => {
   return server.prepare().then(() => nextjsHandle(req, res));
 });
 
+
 exports.weather = require('./functions/weather')
 
-exports.app = functions.region('europe-west1').runWith('128MB').https.onRequest(app);
-app.get('/user/posts/:slug', (req, res) => {
-  const actualPage = '/user/posts'
-  const query = { slug: req.params.slug }
-  app.render(req, res, actualPage, query)
-})
+exports.app = functions.region('europe-west1').runWith('128MB').https.onRequest((app) => {
+  app.get('/user/posts/singlepost/:slug', (req, res) => {
+    console.log('hello');
+    const actualPage = '/user/posts/singlepost'
+    const query = { slug: req.params.slug }
+    app.render(req, res, actualPage, query)
+  })
 
-
-//WeatherAPI calls
-// exports.climaCell = https.onRequest((req, res) => {
-//   cors(req, res, () => {
-// 	// server Express code
-//   })
-// })
-
-// exports.testFunction = https.onRequest((req, res) => {
-// 	if(req.method !== 'GET'){
-// 		return res.status(500).json({
-// 			message: 'Not allowed'
-// 		})
-// 	}
-// 	res.status(200).json({
-// 		message: 'It worked!'
-// 	})
-// })
-
-// exports.freeWeather = https.onRequest( (req, res) => {
-// 	cors(req, res, async () => {
-// 		if(req.method === 'GET'){
-// 			await fetch(config.free_weather_test.testurl, {method: 'GET'})
-// 			.then(response => response.json())
-// 			.then(data => {
-// 				res.status(200).send(data)
-// 			})
-// 			.catch(error => {
-// 				res.status(500).json({
-// 					message: 'Something went wrong while contacting API',
-// 					error: error
-// 				})
-// 			})
-// 		}
-// 		else {
-// 			res.status(500).json({
-// 				message: `${req.method} is not allowed`
-// 			})
-// 		}
-// 	})
-	
-// 			// https://europe-west1-dark-traveling-marshmallow.cloudfunctions.net/weatherStack
-
-	
-// })
+});
