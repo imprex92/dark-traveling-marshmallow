eb3200fc0b52beb418cf87e12f9aa5d7c89d3e14
Daniel Johannesson <daniel@johannesson.cz>
2021-02-19 16:44:42 +0100

    lists all blogs

--- components/BlogList.jsx
+++ components/BlogList.jsx
@@ -1,10 +1,32 @@
-import React from 'react'
+import {useEffect, useState} from 'react'
 import BlogItem from './BlogItem'
 
-const BlogList = () => {
-	return (
-		<div>
+const BlogList = ({searchByText, countrySearchTerm, userBlogs}) => {
+	const [filterByCountry, setFilterByCountry] = useState('')
+	const [fetchedUserBlogs, setFetchedUserBlogs] = useState([])
+	useEffect(() => {
+		setFetchedUserBlogs(userBlogs)
+		setFilterByCountry(countrySearchTerm)
+		return () => {
 			
+		}
+	}, [searchByText, countrySearchTerm, userBlogs])
+
+	let filteredBlogs = fetchedUserBlogs?.filter((post) => {
+		if(!filterByCountry.trim(' '))
+		return post
+		else
+		return post.postLocationData.country.toLowerCase().includes(filterByCountry.toLowerCase())
+	})
+
+	return (
+
+		<div className="row post-item-wrapper">
+			{filteredBlogs.map((blog) => (
+				<div key={blog.id} style={{backgroundImage: `url(${blog.imgURL})`}} className="all-post-backgroundimg">
+					<BlogItem blog={blog} />
+				</div>
+			))}
 		</div>
 	)
 }
