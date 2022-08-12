494427d441979e28356b703ab363d6590dad19fa
Daniel Johannesson <daniel@johannesson.cz>
2022-08-12 16:07:41 +0200

    Extracted geolocation to own component

--- components/utility/GetGeolocation.js
+++ components/utility/GetGeolocation.js
@@ -1,20 +1,26 @@
-export const getGeolocation = () => {
-	let status = {message: '', latitude: '', longitude: ''}
-	if(navigator.geolocation){
-		console.log("Geolocation is avaliable");
-		status.message = "locating you..."
-		navigator.geolocation.getCurrentPosition(position => {
-			console.log(position);
-			return position
-		})
-	}
-	else{
-		status.message = "Your browser doesn't support Geolocation"
-		return status
-	}
-}
+import axios from 'axios'
 
-
-export const reverstGeolocation = () => {
-
+export function getGeolocation(){
+	return new Promise((resolve, reject) => {
+		if(navigator.geolocation){
+			navigator.geolocation.getCurrentPosition(position => {
+				const userLanguage = navigator.language || navigator.userLanguage;
+				const lang = userLanguage.split('-')[0]
+				let latitude = position.coords.latitude;
+				let longitude = position.coords.longitude;
+				const baseURL = "https://api.bigdatacloud.net/data/reverse-geocode-client"
+				let fullURL = baseURL + `?latitude=${latitude}&longitude=${longitude}&localityLanguage=${lang}`
+				axios.get(fullURL)
+				.then(result => {
+					console.log("reverse OK", result);
+					const reversedGeoData = result;
+					console.log('result', reversedGeoData);
+					resolve(reversedGeoData)
+				})
+				.catch(err => {
+					reject({isSuccess: false, message: err})
+				})
+			})
+		}
+	})
 }
