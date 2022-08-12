3d90e80d49558212954f164ea3f8ba2d9986a73b
Daniel Johannesson <daniel@johannesson.cz>
2022-08-12 16:08:09 +0200

    init created weateh page + component

---
+++ components/WeatherMaster.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const WeatherMaster = () => {
+  return (
+	<div>WeatherMaster</div>
+  )
+}
+
+export default WeatherMaster
--- pages/weather.js
+++ pages/weather.js
@@ -1,10 +1,11 @@
 import React from 'react'
+import WeatherMaster from '../components/WeatherMaster'
 
 const weather = () => {
 	return (
 		<div>
 			<span>This is the weather page</span>
-			
+			<WeatherMaster/>
 		</div>
 	)
 }
