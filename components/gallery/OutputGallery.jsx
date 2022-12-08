2c6fe762acb39ebb70c7ea3376a379f0f9a42771
Daniel Bréelle Johannesson <56023250+imprex92@users.noreply.github.com>
2022-12-08 14:18:41 +0100

    init component outputting gallery

---
+++ components/gallery/OutputGallery.jsx
@@ -0,0 +1,114 @@
+import React, {useState, useEffect} from 'react'
+import Image from 'next/image'
+import DateFormatter from 'components/utility/DateFormatter'
+import Gallerypopup from 'components/popups/Gallerypopup'
+
+const OutputGallery = ({data}) => {
+  const {id, title, imgURL, pickedDateForPost, locationData} = data?.elm
+  const [popupToOpen, setPopupToOpen] = useState(null)
+
+  const toggleDropdown = ({event, trigger}) => {
+    event.stopPropagation();
+    if(trigger === 'dropdown'){
+      console.log('dropDown');
+      const i = event.target.classList[1]
+      console.log(event.target.classList[1]);
+      const el = document.getElementsByClassName(`hidden-section-${i}`)
+      const el2 = document.getElementsByClassName(`dropdown-${i}`) 
+      el[0].classList.toggle('showing')
+      el2[0].classList.toggle('active')
+    }
+  }
+  
+
+  const popupTrigger = ({event, trigger, additional}) =>{
+    event.stopPropagation();
+    if(trigger === 'popup'){
+      console.log('Trigger', additional);
+      setPopupToOpen(additional.index)
+    }
+  }
+  function reset(e){
+    
+    console.log('reseting');
+   setPopupToOpen(null)
+    console.log(popupToOpen);
+  }
+
+  return (
+    <div className={`item-container`} onClick={(e) => {popupTrigger({
+      event: e, 
+      trigger:'popup',
+      additional: {id: id, index: data.index}
+      })}}>
+        { popupToOpen !== null  ? <div onClick={(e) => {e.stopPropagation(),reset(e)}} className={`overlay ${popupToOpen ? 'open' : ''}`}>
+      <Gallerypopup key={data.index}  data={data} resetPopup={() => reset()} popupToOpen={popupToOpen}/>
+      </div> : null}
+      <div className='item-image'>
+        <Image 
+          src={imgURL} 
+          quality={50} 
+          layout="responsive"
+          width="50%"
+          height="50%"
+          objectFit='cover' 
+          />
+      </div>
+      <div className='item-text'>
+        <span className='title'>{title}</span>
+        <span className="date">
+          Date: 
+          { pickedDateForPost?.seconds ? 
+          <DateFormatter 
+          timeFromNow={false} 
+          timestamp={pickedDateForPost.seconds} /> : null }
+        </span>
+        <div className={`${data?.index} dropdown-${data?.index}`} onClick={(e) => toggleDropdown({event: e, trigger: 'dropdown'})}>
+          Show more
+          <i className={`${data?.index} material-icons chevron`}>chevron_left</i>
+        </div>
+        <div className={`hidden-section-${data?.index}`}>
+          <span className="country">Country: {locationData?.countryOfPurchase}</span>
+          <span className="store">Store: {locationData?.storeOfPurchase}</span>
+        </div>
+      </div>
+      <style jsx scoped>{`
+        .dropdown-${data?.index}{
+          display: flex;
+          cursor: pointer;
+        }
+        .dropdown-${data?.index} .chevron{
+          margin: 0 0 0 auto;
+          transform: rotate(0deg);
+          transition: transform 0.3s linear;
+        }
+      .dropdown-${data?.index}.active .chevron{
+        transform: rotate(-90deg);
+        transition: transform 0.3s linear;
+      }
+        .item-container{
+
+        }
+        .item-text{
+          display: flex;
+          flex-direction: column;
+          margin: 1rem;
+        }
+        .item-image img{
+          object-fit: cover;
+        }
+        .overlay{
+				width: 100%;
+				height: 100%;
+				position: absolute;
+				top: 0;
+				left: 0;
+				z-index: 3;
+				backdrop-filter: blur(35px);
+			}
+      `}</style>
+    </div>
+  )
+}
+
+export default OutputGallery
