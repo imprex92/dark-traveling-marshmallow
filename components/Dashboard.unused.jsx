5f4ec32555111f1309390790c6ff9d35d67cb29a
Daniel Bréelle Johannesson <56023250+imprex92@users.noreply.github.com>
2023-01-23 18:25:16 +0100

    Update Dashboard.unused.jsx

--- components/Dashboard.unused.jsx
+++ components/Dashboard.unused.jsx
@@ -14,7 +14,6 @@ function Dashboard() {
 	const [searchText, setSearchText] = useState('')
 
 	useEffect(() => {
-		console.log('frferferf');
 		if(currentUser){
 			let userName = currentUser.displayName
 			let firstName = userName.split(' ')[0];
