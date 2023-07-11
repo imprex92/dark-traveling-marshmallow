9fccad079f747ee5d16996e6f6f669d93b9bca49
Daniel Bréelle Johannesson <56023250+imprex92@users.noreply.github.com>
2023-07-11 16:02:29 +0200

    added gdpr-page

---
+++ pages/GDPR.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const GDPR = () => {
+  return (
+	<><script id="CookieDeclaration" src="https://consent.cookiebot.com/f5fc7d0f-f0b3-46eb-b262-434f483563e1/cd.js" type="text/javascript" async></script></>
+  )
+}
+
+export default GDPR
