c00c1cc3e956cf7de52e771b0e32526674da05c5
Daniel Bréelle Johannesson <56023250+imprex92@users.noreply.github.com>
2023-07-11 14:59:27 +0200

    created onlineStatus Hook

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,6 @@
 import Head from 'next/head'
 import Script from 'next/script'
-import { lazy } from 'react'
+import { lazy, useEffect } from 'react'
 import { AuthProvider } from 'contexts/AuthContext'
 import { DatabaseProvider } from 'contexts/DatabaseContext'
 import 'styles/global.style.css'
@@ -14,7 +14,7 @@ import 'styles/singlePost.style.css'
 import 'styles/weather.style.css'
 import useSiteSettings from 'store/siteSettings';
 import {useRouter} from 'next/router';
-import { Offline, Online } from "react-detect-offline";
+import { useOnlineStatus } from 'components/hooks/useOnlineStatus'
 import OfflineComp from 'components/OfflineComp'
 const WeatherWidget = lazy(() => import('components/widgets/WeatherWidget'))
 
@@ -28,6 +28,11 @@ function MyApp({Component,pageProps}) {
 	const widgetProhibited = ['/login', '/signup', '/']
 	const router = useRouter()
 	const { showWeaterWidget } = useSiteSettings(state => state.data)
+	const isOnline = useOnlineStatus()
+
+	useEffect(() => {
+
+	}, [])
 
   return (
 	  <>
@@ -37,6 +42,16 @@ function MyApp({Component,pageProps}) {
 					<meta charSet="utf-8" />
 					<meta name="viewport" content="width=device-width, initial-scale=1"></meta>
 				</Head>
+				<Script id="google-tag-manager" strategy="afterInteractive">
+					{`
+						(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+						new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+						j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+						'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+						})(window,document,'script','dataLayer','${process.env.GTM_ID}');
+					`}
+				</Script>
+				<Script strategy="afterInteractive" id="Cookiebot" src="https://consent.cookiebot.com/uc.js" data-cbid="f5fc7d0f-f0b3-46eb-b262-434f483563e1" type="text/javascript" async/>
 				<Script strategy="afterInteractive" src={`https://www.googletagmanager.com/gtag/js?id=${process.env.PROJECT_FIREBASE_MEASUREMENT_ID}`}/>
 				<Script
 				id='google-analytics'
@@ -52,13 +67,8 @@ function MyApp({Component,pageProps}) {
 				`,
 				}}
 				/>
-				<Online>
-					{ showWeaterWidget && !widgetProhibited.includes(router.pathname) ? <WeatherWidget /> : null}
-					<Component {...pageProps}/>
-				</Online>
-				<Offline>
-					<OfflineComp />
-				</Offline>
+					{ isOnline && showWeaterWidget && !widgetProhibited.includes(router.pathname) ? <WeatherWidget /> : null}
+					{ isOnline ? <Component {...pageProps}/> : <OfflineComp />}
 			{/* </DatabaseProvider> */}
 		</AuthProvider>
 	  </>
