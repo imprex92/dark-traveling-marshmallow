d198057c2f92842f985690642bd6fc4b55bfb2f3
Daniel Bréelle Johannesson <56023250+imprex92@users.noreply.github.com>
2023-10-20 11:57:31 +0200

    updated to new Link component

--- components/BlogItem.jsx
+++ components/BlogItem.jsx
@@ -13,33 +13,34 @@ const BlogItem = ({blog}) => {
 		return text.slice(0, num) + "..."
 	}	
 
-	return (
-		<>
-			<Link as={`/user/posts/post/${blog.slug}`} href={`/user/posts/post?slug=${blog.slug}`}>
-				<div  className={styles.postItemWrapper}>
-					<div className={styles.postTextContent}>
-						<h4 className="postTitle center-align">
-							{blog.postTitle ?? 'No title'}
-						</h4>
-						<div className={styles.postInfo}>
-							<p className={styles.postContent}>
-								{blog.postContent ? textSlicer(blog.postContent, 50) : 'No text'}
-							</p>
-							{ blog.createdByUser && 
-								<>
-									<Link href="#">
-										<a className={styles.userLink}>@{blog.createdByUser}</a> 
-									</Link>
-									<br/>
-								</> 
-							}
-							<DateFormatter timeFromNow={false} timestamp={blog.timestamp?.seconds}/>
-						</div>
-					</div>
-				</div>
-			</Link>
-		</>
-	)
+	return <>
+        <Link
+            as={`/user/posts/post/${blog.slug}`}
+            href={`/user/posts/post?slug=${blog.slug}`}
+            legacyBehavior>
+            <div  className={styles.postItemWrapper}>
+                <div className={styles.postTextContent}>
+                    <h4 className="postTitle center-align">
+                        {blog.postTitle ?? 'No title'}
+                    </h4>
+                    <div className={styles.postInfo}>
+                        <p className={styles.postContent}>
+                            {blog.postContent ? textSlicer(blog.postContent, 50) : 'No text'}
+                        </p>
+                        { blog.createdByUser && 
+                            <>
+                                <Link href="#" className={styles.userLink}>
+                                    @{blog.createdByUser}
+                                </Link>
+                                <br/>
+                            </> 
+                        }
+                        <DateFormatter timeFromNow={false} timestamp={blog.timestamp?.seconds}/>
+                    </div>
+                </div>
+            </div>
+        </Link>
+    </>;
 }
 
 export default BlogItem
