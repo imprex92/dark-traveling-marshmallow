d42117ba159892df0d3ce00036284e20ccccf32f
Daniel Bréelle Johannesson <56023250+imprex92@users.noreply.github.com>
2024-02-21 21:14:52 +0100

    Refactor AddPostForm component and update styles

--- components/AddPostForm.jsx
+++ components/AddPostForm.jsx
@@ -5,12 +5,14 @@ import countriesByContinent from './utility/countries_new'
 import PlacesAutocomplete from './utility/GooglePlacesAutocomplete'
 
 const AddPostForm = props => {
-  const [activeTab, setActiveTab] = useState('start')
+  const [activeTab, setActiveTab] = useState('content')
   const [countries, setCountries] = useState(countriesByContinent)
   const [countryCode, setCountryCode] = useState(null)
   const placesInputValue = useRef('')
   const postCountry = useRef(null)
   const selectRef = useRef(null)
+  const postMood = useRef(null)
+  const postWeather = useRef(null)
 
 	const [datePicker, setDatePicker] = useState(new Date())
 	const [postLocation, setPostLocation] = useState([])
@@ -51,14 +53,11 @@ const AddPostForm = props => {
     <>
     <div className={`${styles.stepTabs}`}>
       <div className={styles.navList}>
-        <div onClick={(e) => {handleTabClick('start', e)}} className={`${activeTab === 'start' ? styles.tab_active : styles.tab}`}>
-          <span className={`material-symbols-outlined ${styles.label}`}>line_start_circle</span>Start
+        <div onClick={(e) => {handleTabClick('content', e)}} className={`${activeTab === 'content' ? styles.tab_active : styles.tab}`}>
+          <span className={`material-symbols-outlined ${styles.label}`}>line_start_circle</span>Content
         </div>
         <div onClick={(e) => {handleTabClick('images', e)}}  className={`${activeTab === 'images' ? styles.tab_active : styles.tab}`}>
           <span className={`material-symbols-outlined ${styles.label}`}>attach_file_add</span>Image
-        </div>
-        <div onClick={(e) => {handleTabClick('content', e)}} className={`${activeTab === 'content' ? styles.tab_active : styles.tab}`}>
-          <span className={`material-symbols-outlined ${styles.label}`}>edit_document</span>Content
         </div>
         <div onClick={(e) => {handleTabClick('submit', e)}} className={`${activeTab === 'submit' ? styles.tab_active : styles.tab}`}>
           <span className={`material-symbols-outlined ${styles.label}`}>task_alt</span>Submit
@@ -67,13 +66,13 @@ const AddPostForm = props => {
 
       <form onSubmit={handleFormSubmit} className={`${styles.postForm}`}>
         <div className={`${styles.tabContent}`}>
-          <div className={`${activeTab === 'start' ? styles.tabPanel_active : styles.tabPanel}`}>
+          <div className={`${activeTab === 'content' ? styles.tabPanel_active : styles.tabPanel}`}>
             <h2 className="">Start of post</h2>
             <div className={styles.fieldGroup}>
               <div className={`input-field ${styles.selectRow}`}>
                 <i className="material-icons white-text">public</i>
                 <select defaultValue="" id="countrySelect" onChange={handleCountryChange} ref={selectRef}>
-                  <option value="" disabled></option>
+                  <option value="" disabled>Select country</option>
                   {Object.entries(countries).map(([continent, countriesList]) => (
                     <optgroup key={continent} label={continent}>
                       {countriesList.map((country) => (
@@ -92,15 +91,38 @@ const AddPostForm = props => {
               <div className="input-field col s11 l6 datepicker-section">
                 <i className="material-icons prefix white-text">event</i>
                 <input id="datepicker" type="text" className="datepicker"/>
-                <label htmlFor="datepicker">Select date</label>
+                <label htmlFor="datepicker">Select journey date</label>
+              </div>
+            </div>
+            <div className={styles.fieldGroup}>
+              <div className="input-field col s11 l6">
+                <i className="material-icons prefix white-text">mood</i>
+                <input ref={postMood} type="text" id="postMood" className="validate" placeholder=' ' />
+                <label htmlFor="postMood">How do you feel right now?</label>
+              </div>
+              <div className="input-field col s11 l6">
+                <i className="material-icons prefix white-text">nights_stay</i>
+                <input ref={postWeather} type="text" id="postWeather" className="validate" placeholder=' ' />
+                <label htmlFor="postWeather">How's the weather today?</label>
+              </div>
+            </div>
+            <div className={styles.fieldGroup}>
+              <div className="input-field col s6">
+                <i class="material-icons prefix white-text">title</i>
+                <input id="postTitle" type="email" class="validate" placeholder=" " />
+                <label htmlFor="postTitle">Title</label>
+              </div>
+            </div>
+            <div className={styles.fieldRow}>
+              <div className="input-field col s12">
+                <i class="material-icons prefix white-text">mode_edit</i>
+                <textarea id="postContent" className="materialize-textarea" placeholder=" "></textarea>
+                <label for="postContent">Write something for your  post...</label>
               </div>
             </div>
           </div>
           <div className={`${activeTab === 'images' ? styles.tabPanel_active : styles.tabPanel}`}>
             <h2>Add pictures here</h2>
-          </div>
-          <div className={`${activeTab === 'content' ? styles.tabPanel_active : styles.tabPanel}`}>
-            <h2>Add content here</h2>
           </div>
           <div className={`${activeTab === 'submit' ? styles.tabPanel_active : styles.tabPanel}`}>
             <h2>submit if all OK</h2>
