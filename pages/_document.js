0702457576cb1baed5f1f35e8a3afa84ac0aa92c
Daniel Bréelle Johannesson <56023250+imprex92@users.noreply.github.com>
2024-06-26 16:35:49 +0200

    Update Materialize to 2.1.0

--- pages/_document.js
+++ pages/_document.js
@@ -16,7 +16,7 @@ class MyDocument extends Document {
         <body>
           <Main />
           <NextScript />
-          <script src="https://cdn.jsdelivr.net/npm/@materializecss/materialize@2.0.4/dist/js/materialize.min.js"/>
+          <script src="https://cdn.jsdelivr.net/npm/@materializecss/materialize@2.1.0/dist/js/materialize.min.js"/>
         </body>
       </Html>
     )
